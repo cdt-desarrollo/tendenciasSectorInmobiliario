ec8e820dd263c27947642201eb0348aca8994d77
Jair Hernández <startupstijuana@gmail.com>
2023-04-18 14:00:15 -0700

    Update

--- main.js
+++ main.js
@@ -14,110 +14,120 @@ function getValues() {
   }
   data.push(companyName, staffName, email, phone)
   console.log(data)
-  if(data[21] == "")
-  {
+  console.log(data.length)
+  if(data.length == 24){
+    if(data[21] == "")
+    {
+      Swal.fire({
+        icon: "info",
+        title: "Falta que escribas el nombre de tu empresa",
+        confirmButtonColor: "#3085d6"
+      })
+      // async() => {
+      //   const { value: accept} =
+      //   await Swal.fire({
+      //     icon: "info",
+      //     title: "Falta escribir tu nombre",
+      //     confirmButtonColor: "#3085d6",
+      //   })
+      //   if(accept){
+      //     location.reload()
+      //   };
+      // }
+    }
+    else if(data[21] != "")
+    {
+      Swal.fire({
+        position: "center",
+        title: "Enviando información",
+        showConfirmButton: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+      });
+      sendData(data)
+    }
+  }
+  else {
     Swal.fire({
-      icon: "info",
-      title: "Falta que escribas el nombre de tu empresa",
+      icon: "error",
+      title: "Faltan respuestas por contestar o que agregues el nombre de tu empresa",
       confirmButtonColor: "#3085d6"
     })
-    // async() => {
-    //   const { value: accept} =
-    //   await Swal.fire({
-    //     icon: "info",
-    //     title: "Falta escribir tu nombre",
-    //     confirmButtonColor: "#3085d6",
-    //   })
-    //   if(accept){
-    //     location.reload()
-    //   };
-    // }
-  }
-  else if(data[21] != "")
-  {
-    Swal.fire({
-      position: "center",
-      title: "Enviando información",
-      showConfirmButton: false,
-      didOpen: () => {
-        Swal.showLoading();
-      },
-    });
-    sendData(data)
   }
 }
 
 function sendData(array) {
-  var data = JSON.stringify({
-    q1: `${array[0]}`,
-    q2: `${array[1]}`,
-    q3: `${array[2]}`,
-    q4: `${array[3]}`,
-    q5: `${array[4]}`,
-    q6: `${array[5]}`,
-    q7: `${array[6]}`,
-    q8: `${array[7]}`,
-    q9: `${array[8]}`,
-    q10: `${array[9]}`,
-    q11: `${array[10]}`,
-    q12: `${array[11]}`,
-    q13: `${array[12]}`,
-    q14: `${array[13]}`,
-    q15: `${array[14]}`,
-    q16: `${array[15]}`,
-    q17: `${array[16]}`,
-    q18: `${array[17]}`,
-    q19: `${array[18]}`,
-    q20: `${array[19]}`,
-    staffName: `${array[20]}`,
-    companyName: `${array[21]}`,
-    email: `${array[22]}`,
-    phone: `${array[23]}`,
-  });
-
-  var config = {
-    method: "post",
-    url: "https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/encuesta2023",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
-
-  axios(config)
-    .then((res) => {
-      // console.log(res)
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Información enviada, ya puede cerrar esta ventana",
-        showConfirmButton: false,
-        confirmButtonColor: "#3085d6",
-      });
-      document.getElementById("deleteDiv").remove();
-      let divToInsertHTMLTags = document.getElementById("divToInsertHTMLTags");
-      let notification = document.createElement("div");
-      notification.innerHTML = `<h4 class="text-center">Gracias por llenar la encuesta, ya puede cerrar esta ventana.</h4>`;
-      divToInsertHTMLTags.insertAdjacentElement("beforeend", notification);
-    })
-    .catch(async(err) => {
-      // const { value: accept} =
-      // await Swal.fire({
-      //   icon: "error",
-      //   title: "¡Algo salió mal!",
-      //   text: "Recuerda que tu nombre es obligatorio",
-      //   confirmButtonColor: "#3085d6",
-      // })
-      // if(accept){
-      //   location.reload()
-      // };
-      Swal.fire({
-        icon: "error",
-        title: "Falta escribir el nombre de tu empresa",
-        confirmButtonColor: "#3085d6"
-      })
+    var data = JSON.stringify({
+      q1: `${array[0]}`,
+      q2: `${array[1]}`,
+      q3: `${array[2]}`,
+      q4: `${array[3]}`,
+      q5: `${array[4]}`,
+      q6: `${array[5]}`,
+      q7: `${array[6]}`,
+      q8: `${array[7]}`,
+      q9: `${array[8]}`,
+      q10: `${array[9]}`,
+      q11: `${array[10]}`,
+      q12: `${array[11]}`,
+      q13: `${array[12]}`,
+      q14: `${array[13]}`,
+      q15: `${array[14]}`,
+      q16: `${array[15]}`,
+      q17: `${array[16]}`,
+      q18: `${array[17]}`,
+      q19: `${array[18]}`,
+      q20: `${array[19]}`,
+      staffName: `${array[20]}`,
+      companyName: `${array[21]}`,
+      email: `${array[22]}`,
+      phone: `${array[23]}`,
     });
+  
+    var config = {
+      method: "post",
+      url: "https://sheet.best/api/sheets/32aacf91-902e-4e88-8d70-e596b2f41ceb/tabs/encuesta2023",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+  
+    axios(config)
+      .then((res) => {
+        // console.log(res)
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Información enviada, ya puede cerrar esta ventana",
+          showConfirmButton: false,
+          confirmButtonColor: "#3085d6",
+        });
+        document.getElementById("deleteDiv").remove();
+        let divToInsertHTMLTags = document.getElementById("divToInsertHTMLTags");
+        let notification = document.createElement("div");
+        notification.innerHTML = `<h4 class="text-center">Gracias por llenar la encuesta, ya puede cerrar esta ventana.</h4>`;
+        divToInsertHTMLTags.insertAdjacentElement("beforeend", notification);
+      })
+      .catch(async(err) => {
+        // const { value: accept} =
+        // await Swal.fire({
+        //   icon: "error",
+        //   title: "¡Algo salió mal!",
+        //   text: "Recuerda que tu nombre es obligatorio",
+        //   confirmButtonColor: "#3085d6",
+        // })
+        // if(accept){
+        //   location.reload()
+        // };
+        Swal.fire({
+          icon: "error",
+          title: "Falta escribir el nombre de tu empresa",
+          confirmButtonColor: "#3085d6"
+        })
+      });
 }
 
 function escribirCiudad() {
